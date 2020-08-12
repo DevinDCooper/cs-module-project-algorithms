e2c3b0a33ac0a0f622c7dfa0da90fc13081b0f6d
Devin Cooper <37602228+DevinDCooper@users.noreply.github.com>
2020-08-11 19:51:06 -0700

    single_number_moving_zeroes_done

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -4,9 +4,16 @@ Returns: a List of integers
 '''
 def moving_zeroes(arr):
     # Your code here
+    count = 0
 
-    pass
-
+    for i in range(len(arr)):
+        if arr[i] != 0:
+            arr[count] = arr[i]
+            count+= 1
+    while count < len(arr):
+        arr[count] = 0
+        count += 1
+    return arr
 
 if __name__ == '__main__':
     # Use the main function here to test out your implementation
